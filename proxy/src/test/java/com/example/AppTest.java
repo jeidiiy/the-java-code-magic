d2e6df9a7f6e4cacc3b0b0c8d5b20f425dbe19e3
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-09-28 18:13:36 +0900

    feat: 동적 프록시 실습

--- proxy/src/test/java/com/example/AppTest.java
+++ proxy/src/test/java/com/example/AppTest.java
@@ -10,7 +10,14 @@ import org.junit.Test;
 public class AppTest {
     @Test
     public void proxy() {
-        BookService bookService = (BookService) Proxy.newProxyInstance(BookService.class.getClassLoader(), new Class[]{BookService.class}, (proxy, method, args) -> null);
+        BookService bookService = (BookService) Proxy.newProxyInstance(BookService.class.getClassLoader(),
+                new Class[] { BookService.class }, (proxy, method, args) -> {
+                    BookService service = new DefaultBookService();
+                    System.out.println("AAAA");
+                    Object invoke = method.invoke(service, args);
+                    System.out.println("BBBB");
+                    return invoke;
+                });
         Book book = new Book("Demian", "Hello Demian", "Hermann Hesse");
 
         bookService.rent(book);
