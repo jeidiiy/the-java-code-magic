8f39c2eba891b1a9de0181a9a864b81048e0ab89
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-09-28 17:53:12 +0900

    env: 자바 버전 및 build 태그 삭제

--- proxy/src/test/java/com/example/AppTest.java
+++ proxy/src/test/java/com/example/AppTest.java
@@ -1,15 +1,16 @@
 package com.example;
 
+import java.lang.reflect.Proxy;
+
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     @Test
     public void proxy() {
-        BookService bookService = new ProxyBookService();
+        BookService bookService = (BookService) Proxy.newProxyInstance(BookService.class.getClassLoader(), new Class[]{BookService.class}, (proxy, method, args) -> null);
         Book book = new Book("Demian", "Hello Demian", "Hermann Hesse");
 
         bookService.rent(book);
